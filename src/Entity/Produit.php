e730e5a717b5b37a42bb98768663e28e8ffcf66c
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-04-30 05:20:57 +0200

    creation de l'entite produit

---
+++ src/Entity/Produit.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $duree_de_conservation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDureeDeConservation(): ?int
+    {
+        return $this->duree_de_conservation;
+    }
+
+    public function setDureeDeConservation(int $duree_de_conservation): self
+    {
+        $this->duree_de_conservation = $duree_de_conservation;
+
+        return $this;
+    }
+}
